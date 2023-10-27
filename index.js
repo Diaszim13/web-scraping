6bfddde028943abfa6ea8c1bce4db007e3746861
Matheus Dias <53883196+Diaszim13@users.noreply.github.com>
2023-10-26 23:32:59 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,66 @@
+const puppeteer = require('puppeteer');
+const XLSX = require('xlsx');
+
+
+const link = XLSX.readFile('./arquivos antigos/01 404PROMORA FGTS C6 20 - 10 .xlsx');
+
+const worksheet = link.Sheets[link.SheetNames[1]];
+
+const data = XLSX.utils.sheet_to_json(worksheet);
+
+arr = Object.values(data);
+
+/**
+  * DATA
+*   {
+    NOME: 'CARLOS EDUARDO HABY DE BAIRROS',
+    CPF: '050.603.099-75',
+    CELULAR: '(51)982451824',
+    'Valor_Bruto\\': '1.405,38 \\',
+    'Valor_Liberado\\': '765,32 \\',
+    monitor: ' OBSERVACAO: \\',
+    data_registro: '1074 Monitor nome:01 404PROMORA FGTS C6 07.10',
+    __EMPTY: 45207.8328125
+  },
+  * */ 
+
+(async () => {
+    // Inicializar o Puppeteer
+    const browser = await puppeteer.launch({ headless: false });
+    const page = await browser.newPage();
+
+    // Navegar até a página desejada
+    await page.goto('https://simulador.404promotora.com.br/page/fgts/simulation/stage-1/indication/8e1f0c6f-aa60-4da3-a07b-47954e147634');
+
+    // Esperar a página carregar completamente
+    await page.waitForSelector('#name');
+
+    await page.type('#name',"matheus dias");
+    await page.type("#cpf", "08312694994");
+    await page.type("#birthDate", "20/05/2000");
+    await page.type("#whatsappNumber", arr[0].CELULAR);
+    await page.click("#term");
+
+    const name = await page.$eval("#name", input => input.value);
+    const cpf = await page.$eval("#cpf", input => input.value);
+    const aniversario = await page.$eval("#birthDate", input => input.value);
+    const whats = await page.$eval("#whatsappNumber", input => input.value);
+    const checkbox = await page.$eval("#term", input => input.value);
+
+
+    await page.click("#btn-simulation");
+
+  console.log(name, cpf, aniversario, whats, checkbox);
+  
+  await Promise.all([
+    page.waitForNavigation(),
+    // page.click("#btn-simulation"),
+  ]);
+
+  const newPage = await browser.pages().then(page => page[page.length - 1]);
+  console.log(newPage.url());
+  
+
+    // Fechar o navegador
+    // await browser.close();
+})();
