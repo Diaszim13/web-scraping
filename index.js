9cfba7157a0b15299bb22a6796a7cd425affdd6d
Matheus Dias <53883196+Diaszim13@users.noreply.github.com>
2023-10-27 19:54:32 -0300

    correções

--- index.js
+++ index.js
@@ -12,34 +12,28 @@ const data = XLSX.utils.sheet_to_json(worksheet);
 
 arr = Object.values(data);
 
+(async () => {
+  // Inicializar o Puppeteer
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
 
-interface tipeResult {
-  text: String,
-  content: String
-}
-
-(async request () => {
-    // Inicializar o Puppeteer
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-
-    // Navegar até a página desejada
-    await page.goto(weblink, { waitUntil: 'networkidle2' });
+  // Navegar até a página desejada
+  await page.goto(weblink, { waitUntil: 'networkidle2' });
     
-    await page.type('#name',"matheus dias");
-    await page.type("#cpf", process.env.CPF);
-    await page.type("#birthDate", "20/05/2000");
-    await page.type("#whatsappNumber", arr[0].CELULAR);
-    await page.click("#term");
+  await page.type('#name', "matheus dias");
+  await page.type("#cpf", process.env.CPF);
+  await page.type("#birthDate", "20/05/2000");
+  await page.type("#whatsappNumber", arr[0].CELULAR);
+  await page.click("#term");
 
-    const name = await page.$eval("#name", input => input.value);
-    const cpf = await page.$eval("#cpf", input => input.value);
-    const aniversario = await page.$eval("#birthDate", input => input.value);
-    const whats = await page.$eval("#whatsappNumber", input => input.value);
-    const checkbox = await page.$eval("#term", input => input.value);
+  const name = await page.$eval("#name", input => input.value);
+  const cpf = await page.$eval("#cpf", input => input.value);
+  const aniversario = await page.$eval("#birthDate", input => input.value);
+  const whats = await page.$eval("#whatsappNumber", input => input.value);
+  const checkbox = await page.$eval("#term", input => input.value);
 
 
-    await page.click("#btn-simulation");
+  await page.click("#btn-simulation");
 
   
   await Promise.all([
@@ -54,21 +48,19 @@ interface tipeResult {
    * */
 
   //TODO preciso saber quais sao os ids dos campos para preencher
-  const result = await newPage.evaluate(() => {
+  const result = await newPage.evaluate(async () => {
     const data = document.querySelector("#result").innerText;
     
     const tags = await newPage.$$('div');
 
-    if(typeof data == 'text')
-    {
-      return data;  	
-    }else {
-      continue;
-    }  
+    if (typeof data == 'text') {
+      return data;
+    } else {
+      // continue;
+    }
   });
   
-  if(result)
-  {
+  if (result) {
     const coon = mysql.createConnection({
       host: proccess.env.db_server,
       user: proccess.env.db_user,
@@ -77,7 +69,7 @@ interface tipeResult {
     });
 
     coon.connect((err) => {
-      if(err) return false;
+      if (err) return false;
 
       console.log('Conectado com sucesso');
     });
@@ -85,23 +77,23 @@ interface tipeResult {
     //TODO aq preciso pegar os dados e tranformar em um objecto e botar no banco nessa variavel
     //valuesaq
 
-    coon.query('Insert INTO data values (param1, param2, param3, param4)',$valuesaq, (err, result) => {
-      if(err) throw new err return;
+    coon.query('Insert INTO data values (param1, param2, param3, param4)', $valuesaq, (err, result) => {
+      if (err) throw new err;
   
       console.log('Registrado com sucess, ID: ' + result.id);
     });
 
     coon.end((err) => {
-      if(err) throw new err return;
+      if (err) throw new err;
 
       console.log('fechadoa com sucesso');
-  });
+    });
   } else {
     //TODO aq precisa achar um jeito de refazer as requests 
     //ter um delimitador ate pegar.
     request();
   }
 
-    // Fechar o navegador
-    //await browser.close();
-})();
+  // Fechar o navegador
+  //await browser.close();
+}) ()
