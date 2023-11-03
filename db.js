48422f23912bc4dc70e07356ce291a0b2c09dcbc
Matheus Dias <53883196+Diaszim13@users.noreply.github.com>
2023-11-03 00:42:00 -0300

    varias coisas

--- db.js
+++ db.js
@@ -2,26 +2,20 @@ const mysql = require('mysql');
 const env = require('dotenv').config();
 
 
-class Database {
     
-    constructor() {
-        this.coon = mysql.createConnection({
-            host: process.env.db_server,
-            user: process.env.db_user,
-            password: process.env.db_pass,
-            database: process.env.db_db
-        });
+this.coon = mysql.createConnection({
+    host: process.env.db_server,
+    user: process.env.db_user,
+    password: process.env.db_pass,
+    database: process.env.db_db
+});
 
-        this.coon.connect((err) => {
-            if (err) {
-                console.error('Error connecting to database: ' + err.stack);
-                return;
-            }
-
-            console.log('Connected to database with threadId: ' + this.coon.threadId);
-        });
+this.coon.connect((err) => {
+    if (err) {
+        console.error('Error connecting to database: ' + err.stack);
+        return;
     }
-}
 
+    console.log('Connected to database with threadId: ' + this.coon.threadId);
+});
 
-module.exports = Database;
