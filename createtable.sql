1b29196b550b3f07754e989d25baaa11a0f8a504
Matheus Dias <53883196+Diaszim13@users.noreply.github.com>
2023-11-06 01:02:28 -0300

    Adição da tabela

---
+++ createtable.sql
@@ -0,0 +1,7 @@
+CREATE TABLE simulacao (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  febrabanId VARCHAR(255),
+  qtdParc INT,
+  totalCreditLiberty DECIMAL(10, 2),
+  totalCreditAccountFGTS DECIMAL(10, 2)
+);
