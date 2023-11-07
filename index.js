a0fdf5bec854243837bcbe4e42c2fca4b0157e4e
Matheus Dias <53883196+Diaszim13@users.noreply.github.com>
2023-11-06 21:35:24 -0300

    foi

--- index.js
+++ index.js
@@ -6,49 +6,6 @@ const env = require('dotenv').config();
 //LOGGER  
 const Logger = require('./logger.js');
 
-/**
- * 
-O id para acessar a div que tem os dados que serão carregados é esse!
-#table - parcLabel
-Este cpf pega um certo do banco c6bank
-121.212.517 - 74
-
-Assim vem no html os campos que preciso
-
-<div class="col-md-4">
-    <p class="m-0 pt-3">Parcelas</p>
-    <h3 class="m-0 pb-3"><%=register.qtdParc%> anos</h3>
-</div>
-<div class="col-md-4">
-  <p class="m-0 pt-3">Valor total do empréstimo</p>
-  <h3 class="m-0 pb-3">R$ <%=maskMoney(register.totalCreditAccountFGTS.toFixed(2))%></h3>
-</div>
-<div class="col-md-4">
-  <p class="m-0 pt-3">Valor total Liberado</p>
-  <h3 class="m-0 pb-3">R$ <%=maskMoney(register.totalCreditLiberty.toFixed(2))%></h3>
-</div>
-
-
-Este é o html quando da erro antes de fazer a simulação
-<div aria-labelledby="swal2-title" aria-describedby="swal2-html-container" class="swal2-popup swal2-modal swal2-icon-error swal2-show" tabindex="-1" role="dialog" aria-live="assertive" aria-modal="true" style="display: grid;"><button type="button" class="swal2-close" aria-label="Close this dialog" style="display: none;">×</button><ul class="swal2-progress-steps" style="display: none;"></ul><div class="swal2-icon swal2-error swal2-icon-show" style="display: flex;"><span class="swal2-x-mark">
-    <span class="swal2-x-mark-line-left"></span>
-    <span class="swal2-x-mark-line-right"></span>
-  </span>
-</div><img class="swal2-image"
- style="display: none;"><h2 class="swal2-title" id="swal2-title" style="display: block;">Oops...</h2><div class="swal2-html-container"
-  id="swal2-html-container" style="display: block;">[31054]
-   Não foi possível realizar a simulação, tente utilizar outro navegador! [02/11/2023 21:56:31]</div><input id="swal2-input" class="swal2-input" 
-   style="display: none;"><input type="file" class="swal2-file" style="display: none;"><div class="swal2-range" style="display: none;"><input
-    type="range"><output></output></div><select id="swal2-select" class="swal2-select" style="display: none;"></select><div class="swal2-radio"
-     style="display: none;"></div><label class="swal2-checkbox" style="display: none;"><input type="checkbox" id="swal2-checkbox"><span class="swal2-label">
-     </span></label><textarea id="swal2-textarea" class="swal2-textarea" style="display: none;"></textarea><div class="swal2-validation-message" 
-     id="swal2-validation-message" style="display: none;"></div><div class="swal2-actions" style="display: flex;"><div class="swal2-loader"></div><button type="button" 
-     class="swal2-confirm swal2-styled" aria-label="" style="display: inline-block;">OK</button><button type="button" class="swal2-deny swal2-styled" aria-label=""
-      style="display: none;">No</button><button type="button" class="swal2-cancel swal2-styled" aria-label="" style="display: none;">Cancel</button></div><div 
-      class="swal2-footer" style="display: none;"></div><div class="swal2-timer-progress-bar-container"><div class="swal2-timer-progress-bar" style="display: 
-      none;"></div></div></div>
- */
-
 const weblink = process.env.LINK;
 const link = XLSX.readFile(process.env.EXCEL);
 
