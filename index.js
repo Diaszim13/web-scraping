9d506f8afe62f63016771536addac20da2ed8868
Matheus Dias <53883196+Diaszim13@users.noreply.github.com>
2023-11-09 23:06:18 -0300

    Deleção de arquivos e deixar tudo pronto pra funcionar

--- index.js
+++ index.js
@@ -4,33 +4,28 @@ const XLSX = require('xlsx');
 const env = require('dotenv').config();
 
 const RecaptchaPlugin = require('puppeteer-extra-plugin-recaptcha');
-const stelphPlugin = require('puppeteer-extra-plugin-stealth');
+const StelphPlugin = require('puppeteer-extra-plugin-stealth');
 
-puppeteer.use(StealthPlugin());
+puppeteer.use(StelphPlugin());
 //LOGGER  
 const Logger = require('./logger.js');
 
 const weblink = process.env.LINK;
-const link = XLSX.readFile(process.env.EXCEL);
+const link = XLSX.readFile(`./arquivos/${process.env.EXCEL}`);
 
 const worksheet = link.Sheets[link.SheetNames[0]];
 
 const data = XLSX.utils.sheet_to_json(worksheet);
 
 const arr = data.map(Object.values);
-puppeteer.use(stelphPlugin());
+
+// puppeteer.use(stelphPlugin());
 
 (async () => {
-  // Inicializar o Puppeteer
-  // executablePath: `C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe`
-  //C:\\Users\\mathe\\AppData\\Local\\Programs\\Opera\\launcher.exe
-
   const logger = new Logger();
-  const browser = await puppeteer.launch({ headless: false, args: ['--disable-web-security', '--disable-features=IsolateOrigins,site-per-process'] });
+  const browser = await puppeteer.launch({ headless: true, args: ['--disable-web-security', '--disable-features=IsolateOrigins,site-per-process'] });
   let page = await browser.newPage();
 
-  // Navegar até a página desejada
-  // ALGO COM swal2-title e swal2-content
   for (const data of arr) {
     if (data) {
 
@@ -39,7 +34,6 @@ puppeteer.use(stelphPlugin());
         Object.defineProperty(navigator, 'productSub', {get: () =>  '20100101'})
         Object.defineProperty(navigator, 'vendor', {get: () =>  ''})
         Object.defineProperty(navigator, 'oscpu', {get: () =>  'Windows NT 10.0; Win64; x64'})
-
       })
 
       await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Gecko/20100101 Firefox/73.0 Chrome/114.0.0.0 Safari/537.36');
@@ -47,27 +41,12 @@ puppeteer.use(stelphPlugin());
       await page.goto(weblink, { waitUntil: 'networkidle0', timeout: 0 });
 
 
-
-      await puppeteer.use(RecaptchaPlugin({
-          provider: { id: '2captcha', token: process.env.recapcha },
-          visualFeedback: true // colorize reCAPTCHAs (violet = detected, green = solved)
-        })
-      );
-      // let { captchas } = await page.findRecaptchas()
-      // let { solutions } = await page.getRecaptchaSolutions(captchas)
-      // let { solved, error } = await page.enterRecaptchaSolutions(solutions)
-
       await page.setDefaultNavigationTimeout(0);
       if (!browser.isConnected()) {
         logger.info('Pagina fechada com sucesso');
         return false;
       }
-      // process.env.CPF = data.CPF;
 
-      // let val = await page.waitForSelector('#recaptcha-token', { timeout: 0 });
-
-        //await page.solveRecaptchas()
-      
       let dados = {
         nome: data[0],
         cpf: data[1],
@@ -75,14 +54,6 @@ puppeteer.use(stelphPlugin());
         celular: data[3]
       }
 
-      if(dados['cpf'].length  < 11){
-        for(let i = dados['cpf'].length; i < 11; i++){
-          dados['cpf'] = '0' + dados['cpf'];
-        }
-        // logger.error('CPF invalido: ' + dados['cpf']);
-        // await page.close();
-        // continue;
-      }
 
       const date = new Date(dados['nascimento']);
       
@@ -106,9 +77,8 @@ puppeteer.use(stelphPlugin());
       try {
         await Promise.all([
           page.waitForSelector(".swal2-x-mark", { timeout: 10000 })
-        ],
-          // { waitUntil: 'networkidle2', timeout: 0 }
-        );
+        ]);
+        
         const err = await page.evaluate(() => {
           return document.querySelector(".swal2-x-mark");
         });
@@ -116,10 +86,13 @@ puppeteer.use(stelphPlugin());
         if (err != null) {
           logger.error('Não foi possivel encontrar para o cliente: ' + dados["nome"]);
         }
+
+
         if (!browser.isConnected()) {
           logger.info('Pagina fechada com sucesso');
           return false;
         }
+
         continue;
 
       } catch (Exception) {
